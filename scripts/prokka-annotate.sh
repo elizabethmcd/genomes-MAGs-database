5fe82509a33bcc48382235768bb7c0b255ef790c
Elizabeth McDaniel <emcdaniel@scarcity-cm.glbrc.org>
2019-07-25 14:34:21 -0500

    updates to prokka GLBRC path

--- scripts/prokka-annotate.sh
+++ scripts/prokka-annotate.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+export PROKKA=/opt/bifxapps/prokka
+
+export PATH=$PROKKA/bin:$PATH
+
 for fasta in *.fna; do
     N=$(basename $fasta .fna);
     prokka --outdir $N --prefix $N --cpus 15 $fasta --centre X --compliant;
