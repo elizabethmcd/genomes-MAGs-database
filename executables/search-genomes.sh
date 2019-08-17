92026b1d20fcdaa572698f4a98fd08b8eaf6ceb3
Elizabeth McDaniel <emcdaniel@wisc.edu>
2019-08-16 22:41:47 -0500

    updates to python script for search and executable

--- executables/search-genomes.sh
+++ executables/search-genomes.sh
@@ -7,18 +7,19 @@
 # If there is greater than or equal to 1 hit in the HMM output, write it to a hits file
 # Bring back the hits file with the name of the genome as the title, so can then pull down those genome files isntead of all 200,000 just to do the initial search
 
-# Arguments
-name=$1
-path=$2
+# installations and set path for all programs to work
+tar -xzf anaconda-python.tar.gz
+export PATH=$(pwd)/python/bin:$PATH
 
 # make a working directory so doesn't bring back everything under the sun
 mkdir wd
 mv *.hmm wd
+chmod u+x search-genomes.py
 cd wd
 
 # get the genome file, unzip, rename
 wget $2
-gunzip *.gz 
+gunzip GCA*.gz 
 mv *.fna $1.fna
 
 # call ORFS with prodigl and reformat the headers
@@ -26,4 +27,8 @@ prodigal -i $1.fna -a $1.faa
 sed 's/\s.*$//' $1.faa > $1.faa.fixed
 
 # run HMMsearch and parse results
-python ../search-genome.py $1.faa.fixed 
+# will only create a hits file if the HMMer output contains 1 or more hits, IE not empty
+python ../search-genome.py --genome $1.faa.fixed --marker tfdA.hmm --output $1-hits.faa
+
+# move the hits file
+mv $1-hits.faa /mnt/gluster/emcdaniel/genome-search/.
--- scripts/search-genomes.py
+++ scripts/search-genomes.py
@@ -9,10 +9,12 @@ from Bio import SeqIO, SearchIO
 parser = argparse.ArgumentParser(description = "Search genome for marker of interest")
 parser.add_argument('--genome', metavar='GENOME', help='Protein fasta file of genome to search')
 parser.add_argument('--marker', metavar='MARKER', help="Name of marker to search with")
+parser.add_argument('--output', metavar='OUT', help="Name of output file of hits")
 
 args = parser.parse_args()
-genome = args.genome_dir
+genome = args.genome
 marker = args.marker
+output = args.output
 FNULL = open(os.devnull, 'w')
 prot=os.path.basename(marker).replace(".hmm", "").strip().splitlines()[0]
 
@@ -25,23 +27,16 @@ subprocess.call(cmd, stdout=FNULL)
 
 # Parse HMM file 
 print("Parsing results...")
-result_dir = os.walk("out/"+dir)
-for path, dirs, files in result_dir:
-    for file in files:
-        genome = file.replace(".out", "").strip().splitlines()[0]
-        result = "out/"+dir+"/"+file
-        output="results/"+dir+".faa"
-        genome_file=GENOMEDIR+genome+".faa"
-        with open(output, "a") as outf:
-            with open(genome_file, "r") as input_fasta:
-                with open(result, "r") as input:
-                    for qresult in SearchIO.parse(input, "hmmer3-tab"):
-                        hits = qresult.hits
-                        num_hits = len(hits)
-                        if num_hits>0:
-                            for i in range(0,1):
-                                hit_id=hits[i].id
-                            for record in SeqIO.parse(input_fasta, "fasta"):
-                                if record.id in hit_id:
-                                    outf.write(">"+genome+"\n"+str(record.seq)+"\n")
+with open(outname, "r") as input:
+    with open(genome, "r") as input_fasta:
+        for qresult in SearchIO.parse(input, "hmmer3-tab"):
+            hits= qresult.hits
+            num_hits=len(hits)
+            if num_hits>0:
+                for i in range(0,1):
+                    hit_id = hits[i].id
+                with open(output, "a") as outf:
+                    for record in SeqIO.parse(input_fasta, "fasta"):
+                        if record.id in hit_id:
+                            outf.write(">"+name+" "+record.id+"\n"+str(record.seq)+"\n")
 
