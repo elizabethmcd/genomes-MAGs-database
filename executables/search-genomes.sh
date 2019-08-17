50f4ccdc0cd713c709f54d78addf921ee0d7895d
Elizabeth McDaniel <emcdaniel@wisc.edu>
2019-08-17 13:16:33 -0500

    flip the executable to directly read the queued argument file

--- executables/search-genomes.sh
+++ executables/search-genomes.sh
@@ -7,15 +7,21 @@
 # If there is greater than or equal to 1 hit in the HMM output, write it to a hits file
 # Bring back the hits file with the name of the genome as the title, so can then pull down those genome files isntead of all 200,000 just to do the initial search
 
-# installations and set path for all programs to work
+# installations and setup
 tar -xzf anaconda-python.tar.gz
 export PATH=$(pwd)/python/bin:$PATH
-
-# make a working directory so doesn't bring back everything under the sun
-mkdir wd
-mv *.hmm wd
 chmod u+x search-genomes.py
-cd wd
+
+# set arguments from the list that is queued in from the DAG
+list=`basename $1`
+while read line
+do
+assembly=`echo $line | cut -d' ' -f1`
+ftp=`echo $line | cut -d' ' -f2`
+wget $ftp
+gunzip *.gz
+mv *.fna $assembly.fna
+done < $list
 
 # get the genome file, unzip, rename
 wget $2
@@ -23,12 +29,21 @@ gunzip GCA*.gz
 mv *.fna $1.fna
 
 # call ORFS with prodigl and reformat the headers
-prodigal -i $1.fna -a $1.faa
-sed 's/\s.*$//' $1.faa > $1.faa.fixed
+for file in *.fna; do
+    name=$(basename $file .fna);
+    prodigal -i $file -a $name.faa;
+done
+
+for file in *.faa; do
+    name=$(basename $file .faa);
+    sed 's/\s.*$//' $file > $name.faa.fixed;
+done
 
 # run HMMsearch and parse results
 # will only create a hits file if the HMMer output contains 1 or more hits, IE not empty
-python ../search-genome.py --genome $1.faa.fixed --marker tfdA.hmm --output $1-hits.faa
+for file in *.faa.fixed; do
+    name=$(basename $file .faa.fixed);
+    python ../search-genome.py --genome $file --marker tfdA.hmm --output $name-hits.faa
 
 # move the hits file
 # mv $1-hits.faa /mnt/gluster/emcdaniel/genome-search/.
